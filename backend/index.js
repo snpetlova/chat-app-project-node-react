b1354ba4c9777793fd2e3de863ffabb128f61b8b
Snezhana Petlova <snpetlova@gmail.com>
2024-01-04 11:22:05 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    //This request fetches the user with the following username or creates one from scratch. If the user is created, the respective password will be saved to their account.
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "3936665d-ea3b-4fc6-a0eb-342aeb49ab03" } } //allows us to destroy and create users
+    );
+    return res.status(r.status).json(r.data);
+  } catch (error) {
+    return res.status(error.response.status).json(error.response.data);
+  }
+});
+
+app.listen(3001);
