452bc30fce32a102318576c8862887d1d0954600
Snezhana Petlova <snpetlova@gmail.com>
2024-01-04 12:10:45 +0200

    create the frontend

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,22 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "fe28ace6-1f55-432f-bebd-2c618ca1bd61",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
